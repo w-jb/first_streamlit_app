ab2fdff94ad8a34899976cc25e18555f44f64ee3
w-jb <106088053+w-jb@users.noreply.github.com>
2022-11-09 23:20:41 +0100

    Update streamlit_app.py
    Added Emoji 
    Added 4th line of text

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healty Diner')
 
 streamlit.header('Brekfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
