656379efe04077d466717ca2dfb23487f7d28f47
w-jb <106088053+w-jb@users.noreply.github.com>
2022-11-11 10:59:33 +0100

    Update streamlit_app.py
    Added normalizastion
    Added table form display

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ import requests
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -28,3 +27,8 @@ streamlit.dataframe(fruits_to_show)
 # New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 streamlit.text(fruityvice_response.json())
+
+# Extract raw data from json format
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Display message in table form
+streamlit.dataframe(fruityvice_normalized)
