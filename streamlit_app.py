edcab009f746eaf67cb9b6174c51fd08b9672a8d
w-jb <106088053+w-jb@users.noreply.github.com>
2022-11-11 14:16:57 +0100

    Update streamlit_app.py
    Modified cnx section

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,10 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_row)
+#streamlit.text("The fruit load list contains:")
+
 streamlit.text(my_data_row)
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
