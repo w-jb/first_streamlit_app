1c06f6ee2776593356b8b9adc869b744c6719007
w-jb <106088053+w-jb@users.noreply.github.com>
2022-11-11 11:02:02 +0100

    Update streamlit_app.py
    Commented line 29 t be rmoved from execution
    Modification of fruityvice_response URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -26,7 +26,7 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response.json())
 
 # Extract raw data from json format
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
