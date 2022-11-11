4156947695a5d2b45f1c1f47b93e7ece8c738059
w-jb <106088053+w-jb@users.noreply.github.com>
2022-11-11 14:18:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,10 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
 #streamlit.text("The fruit load list contains:")
 
-streamlit.text(my_data_row)
+#streamlit.text(my_data_row)
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
