997ba4240c1f71f1018a30c80bc570d741580a74
w-jb <106088053+w-jb@users.noreply.github.com>
2022-11-11 15:09:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
+from urllib.error import URLError
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -35,7 +37,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Display message in table form
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -50,5 +52,6 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
+streamlit.stop()
 # Add to DB
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
