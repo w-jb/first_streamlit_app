8589ae9e2868081c0510ac62f5e36b23b4c41968
w-jb <106088053+w-jb@users.noreply.github.com>
2022-11-11 14:57:04 +0100

    Update streamlit_app.py
    Rearange

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
+from urllib.error import URLError
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -37,8 +39,6 @@ streamlit.dataframe(fruityvice_normalized)
 
 # stop point
 streamlit.stop()
-
-import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
